c57e013318756878e37058b7476b04e8be05539d
namchylartysh <artyshnam@gmail.com>
2023-03-29 22:18:42 +0600

    added HelloSpringBoot

---
+++ HelloSpringBoot/src/main/java/spring/repository/PersonRepository.java
@@ -0,0 +1,10 @@
+package spring.repository;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import spring.domain.Person;
+
+@Repository
+public interface PersonRepository extends JpaRepository<Person, Integer> {
+}
