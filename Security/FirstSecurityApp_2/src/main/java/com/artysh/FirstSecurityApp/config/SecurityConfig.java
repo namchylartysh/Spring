1023dd0241b1e7aeb142ee752976cfff7155778e
namchylartysh <artyshnam@gmail.com>
2023-07-28 11:23:59 +0600

    AuthProvider deleted

--- Security/FirstSecurityApp_2/src/main/java/com/artysh/FirstSecurityApp/config/SecurityConfig.java
+++ Security/FirstSecurityApp_2/src/main/java/com/artysh/FirstSecurityApp/config/SecurityConfig.java
@@ -1,23 +1,31 @@
 package com.artysh.FirstSecurityApp.config;
 
-import com.artysh.FirstSecurityApp.security.AuthProviderImpl;
+import com.artysh.FirstSecurityApp.services.PersonDetailsService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
 
 @EnableWebSecurity
 public class SecurityConfig extends WebSecurityConfigurerAdapter {
 
-    private final AuthProviderImpl authProvider;
+    private final PersonDetailsService personDetailsService;
 
     @Autowired
-    public SecurityConfig(AuthProviderImpl authProvider) {
-        this.authProvider = authProvider;
+    public SecurityConfig(PersonDetailsService personDetailsService) {
+        this.personDetailsService = personDetailsService;
     }
 
     // Настраивает аутентификацию
-    protected void configure(AuthenticationManagerBuilder auth) {
-        auth.authenticationProvider(authProvider); // внутрь передаем логику аутентификации
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.userDetailsService(personDetailsService); // вместо своего AuthProvider передаем userDetailsService
+    }
+
+    @Bean
+    public PasswordEncoder getPasswordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
     }
 }
